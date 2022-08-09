badb5a7614abfbd4dd05615b69b07bed01d7768a
David Pérez <dalfonzo18@gmail.com>
2022-08-09 15:44:56 -0400

    Minor changes

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -1,3 +1,33 @@
+const commonBtnStyles = {
+  fontWeight: 'bold',
+  padding: '1rem 2rem',
+  display: 'inline-block',
+  borderRadius: '5px',
+  height: 'unset',
+  width: 'fit-content',
+  _focus: {
+    boxShadow: 'none',
+  },
+  _hover: {
+    boxShadow: 'none',
+    cursor: 'pointer',
+  },
+}
+
+const commonIconBtnStyles = {
+  '& > svg': {
+    mx: '0.3em',
+    fontSize: '1.4em',
+    fill: 'currentColor',
+  },
+  _hover: {
+    transform: 'scale(1.03)',
+  },
+  _active: {
+    transform: 'scale(1.00) translateY(0.1em)',
+  },
+}
+
 export const buttonStyles = {
   components: {
     Button: {
@@ -23,6 +53,26 @@ export const buttonStyles = {
           _hover: {
             boxShadow: 'none',
           },
+        },
+        'btn-primary': {
+          ...commonBtnStyles,
+          background: 'aqua.light',
+          color: 'white',
+          ...commonIconBtnStyles,
+        },
+        'btn-white': {
+          ...commonBtnStyles,
+          background: 'white',
+          ...commonIconBtnStyles,
+          color: 'text.dark',
+        },
+        'btn-white-border': {
+          ...commonBtnStyles,
+          ...commonIconBtnStyles,
+          background: 'white',
+          border: '1px solid',
+          borderColor: 'aqua.light',
+          color: 'aqua.light',
         },
       },
       baseStyle: {
--- theme/components/form.ts
+++ theme/components/form.ts
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 const activeLabelStyles = {
-  // top: 0,
-  transform: 'scale(0.85) translateY(-160%)',
+  transform: 'scale(0.85) translateY(-50%)',
 }
 
 const floatingBaseConfiguration = {
@@ -15,19 +14,24 @@ const floatingBaseConfiguration = {
         boxShadow: '0 0 0 1px #60E0E3',
       },
     },
-    'input:not(:placeholder-shown) + label, .chakra-select__wrapper + label': {
-      ...activeLabelStyles,
-    },
+    'input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) + label':
+      {
+        ...activeLabelStyles,
+      },
+
     'input[aria-invalid=true], input[data-invalid]': {
       borderColor: 'red',
+      _focus: {
+        boxShadow: '0 0 0 1px red',
+      },
     },
     'input[data-invalid] + label, input[aria-invalid=true] + label': {
       color: 'red',
     },
     label: {
-      top: '50%',
+      top: 0,
       left: 0,
-      transform: 'translateY(-50%)',
+      transform: 'translateY(0.6em)',
       position: 'absolute',
       backgroundColor: 'white',
       pointerEvents: 'none',
@@ -36,10 +40,12 @@ const floatingBaseConfiguration = {
       transformOrigin: 'left top',
       zIndex: 1,
       fontSize: '18px',
+      color: 'text.light',
     },
     input: {
       fontSize: '18px',
       padding: '1.5rem 1rem',
+      color: 'text.light',
     },
   },
 }
