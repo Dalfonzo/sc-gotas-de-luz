ed0371bdb9b89406b132a731f51664b57108ece2
David Pérez <dalfonzo18@gmail.com>
2022-08-13 12:56:51 -0400

    Adds hero media queries

--- components/client/landing/card/Card.tsx
+++ components/client/landing/card/Card.tsx
@@ -1,5 +1,6 @@
 import { AspectRatio, Box, Flex, FlexProps, Image, Text } from '@chakra-ui/react'
 import LinkButton from '~/components/client/common/link-button'
+import { responsiveProperty } from '~/theme/utils'
 
 interface CardI extends FlexProps {
   title: string
@@ -12,18 +13,42 @@ interface CardI extends FlexProps {
 }
 
 const Card = ({ title, description, imgSrc, imgAlt, linkHref, rtl, linkText, ...rest }: CardI) => (
-  <Flex maxW="7xl" margin="10rem auto" alignItems="flex-start" {...rest} flexDir={rtl ? 'row-reverse' : 'row'}>
-    <AspectRatio width="40%" ratio={1.7}>
+  <Flex
+    maxW="7xl"
+    mx="auto"
+    my={responsiveProperty({ mobileSize: 3, desktopSize: 10, unit: 'rem' })}
+    alignItems="flex-start"
+    flexDir={{ base: 'column', md: rtl ? 'row-reverse' : 'row' }}
+    {...rest}
+    padding="0 1rem"
+  >
+    <AspectRatio
+      width={{ base: '80%', md: '40%' }}
+      mx={{ base: 'auto', md: 'unset' }}
+      ratio={1.7}
+      marginBottom={{ base: '3rem', md: 'unset' }}
+    >
       <Image src={imgSrc} alt={imgAlt} borderRadius="5px" />
     </AspectRatio>
-    <Box width="50%" margin={rtl ? '0 auto 0 0' : '0 0 0 auto'}>
+    <Box width={{ base: '100%', md: '50%' }} margin={{ base: 'unset', md: rtl ? '0 auto 0 0' : '0 0 0 auto' }}>
       <Text variant="subtitle" marginBottom="2.5rem" as="h2">
         {title}
       </Text>
-      <Text variant="normal" marginBottom="2.5rem">
+      <Text
+        variant="normal"
+        width={{ base: '80%', md: 'unset' }}
+        margin={{ base: '0 auto 2.5rem auto', md: '0 0 2.5rem 0' }}
+      >
         {description}
       </Text>
-      <LinkButton href={linkHref} content={linkText} variant="lnk-btn-primary" />
+      <LinkButton
+        href={linkHref}
+        content={linkText}
+        variant="lnk-btn-primary"
+        margin={{ base: '0 10%  0 auto', md: '0 0 2.5rem 0' }}
+        display={{ base: 'block', md: 'inline-block' }}
+        width={{ base: 'fit-content', md: 'unset' }}
+      />
     </Box>
   </Flex>
 )
--- components/client/landing/hero/Hero.tsx
+++ components/client/landing/hero/Hero.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import { AspectRatio, Box, Flex, Image, Text } from '@chakra-ui/react'
 import LinkButton from '~/components/client/common/link-button'
+import { responsiveProperty } from '~/theme/utils'
 
 const Hero = () => {
   return (
@@ -14,21 +15,64 @@ const Hero = () => {
         transform="translate(-50%, -50%)"
         zIndex="1"
       >
-        <Box width="65%">
-          <Text variant="title" marginBottom="2rem">
+        <Box
+          width={responsiveProperty({ mobileSize: 100, desktopSize: 65, unit: '%' })}
+          display={{ base: 'flex', sm: 'block' }}
+          flexDir={{ base: 'column', sm: 'unset' }}
+          alignItems={{ base: 'center', sm: 'unset' }}
+          justifyContent={{ base: 'center', sm: 'unset' }}
+          padding={{ base: 'unset', sm: '0 1rem' }}
+        >
+          <Text
+            variant="title"
+            marginBottom={responsiveProperty({ mobileSize: 0.5, desktopSize: 2, unit: 'rem' })}
+            textAlign={{ base: 'center', sm: 'unset' }}
+            width={{ base: '90%', sm: '100%' }}
+          >
             Iluminando momentos para generar vida
           </Text>
-          <Text variant="normal" width="60%" fontSize="25px" marginBottom="2.5rem">
+          <Text
+            variant="normal"
+            width={responsiveProperty({ mobileSize: 85, desktopSize: 60, unit: '%' })}
+            fontSize={responsiveProperty({ mobileSize: 14, desktopSize: 25 })}
+            marginBottom={responsiveProperty({ mobileSize: 1, desktopSize: 2.5, unit: 'rem' })}
+            textAlign={{ base: 'center', sm: 'unset' }}
+          >
             Somos un grupo de jóvenes dispuestos a asistir a la población más vulnerable
           </Text>
           <LinkButton href="/donate" variant="lnk-btn-primary" content="Donar" />
         </Box>
       </Box>
       <Box width="50%" />
-      <Box width="50%" position="relative" maxHeight="750px">
-        <Image src="/assets/svg/hero-bg-deco.svg" alt="decoration" position="absolute" top="0" left="0" />
-        <Image src="/assets/img/hero-bg.png" alt="Niños sonriendo" width="100%" height="100%" objectFit="cover" />
-      </Box>
+      <AspectRatio
+        width={responsiveProperty({ mobileSize: 70, desktopSize: 50, unit: '%' })}
+        position="relative"
+        ratio={{ base: 0.6, sm: 0.8 }}
+        maxHeight="750px"
+        pointerEvents="none"
+        overflow="hidden"
+      >
+        <Box>
+          <Image
+            src="/assets/svg/hero-bg-deco.svg"
+            alt="decoration"
+            position="absolute"
+            top="0"
+            left="0"
+            width="40%"
+            maxWidth="250px"
+          />
+          <Image
+            src="/assets/img/hero-bg.png"
+            alt="Mujer sosteniendo a una niña"
+            width="100%"
+            height="100%"
+            objectFit="cover"
+            display={{ base: 'none', sm: 'unset' }}
+            objectPosition="left"
+          />
+        </Box>
+      </AspectRatio>
     </Flex>
   )
 }
