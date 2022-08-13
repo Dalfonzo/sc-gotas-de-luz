1d87e9ad961dc551eb4b75ad7c5d576a52c0aaad
David Pérez <dalfonzo18@gmail.com>
2022-08-13 15:08:29 -0400

    Adds footer mq

--- components/client/common/footer/Footer.tsx
+++ components/client/common/footer/Footer.tsx
@@ -1,23 +1,42 @@
 import { Box, Flex, Image, List, ListItem, Text } from '@chakra-ui/react'
+import { responsiveProperty } from '~/theme/utils'
 
 const Footer = () => {
   return (
     <Box as="footer" color="white" marginTop="8rem">
       <Image src="assets/svg/footer-bg.svg" alt="footer background" width="100%" />
-      <Box padding="5rem 0" background="aqua.light" marginTop="-9vw">
-        <Flex maxW="7xl" margin="auto" fontSize="18px" justifyContent="space-between">
-          <Box display="flex" alignItems="center">
+      <Box padding="5rem 2rem" background="aqua.light" marginTop="-9vw">
+        <Flex
+          maxW="7xl"
+          margin="auto"
+          fontSize={responsiveProperty({ mobileSize: 12, desktopSize: 18 })}
+          justifyContent="space-between"
+          flexDir={{ base: 'column', md: 'row' }}
+          alignItems={{ base: 'center', md: 'unset' }}
+          gap="2rem"
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDir={{ base: 'column', sm: 'row' }}
+            gap={{ base: '1.5rem', sm: 'unset' }}
+          >
             <Image src="assets/svg/logo-footer.svg" alt="gotas de luz" />
             <Box marginLeft="2rem" width="80%">
-              <Text fontSize="25px" fontWeight="bold">
+              <Text fontSize={responsiveProperty({ mobileSize: 14, desktopSize: 25 })} fontWeight="bold">
                 Cambiemos al mundo, juntos.
               </Text>
               <Text>Existen muchas formas en que puedes colaborar con nosotros. </Text>
             </Box>
           </Box>
-          <Flex gap="5rem">
+          <Flex gap={responsiveProperty({ mobileSize: 2, desktopSize: 5, unit: 'rem' })}>
             <List spacing="0.5rem">
-              <ListItem as="li" fontSize="25px" fontWeight="bold" marginBottom="1rem">
+              <ListItem
+                as="li"
+                fontSize={responsiveProperty({ mobileSize: 14, desktopSize: 25 })}
+                fontWeight="bold"
+                marginBottom="1rem"
+              >
                 Sitio
               </ListItem>
               <ListItem>Acerca</ListItem>
@@ -26,7 +45,12 @@ const Footer = () => {
               <ListItem>Eventos</ListItem>
             </List>
             <List spacing="0.5rem">
-              <Box as="li" fontSize="25px" fontWeight="bold" marginBottom="1rem">
+              <Box
+                as="li"
+                fontSize={responsiveProperty({ mobileSize: 14, desktopSize: 25 })}
+                fontWeight="bold"
+                marginBottom="1rem"
+              >
                 Contacto
               </Box>
               <ListItem>@gotasdeluz.ve</ListItem>
