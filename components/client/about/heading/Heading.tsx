6eb5a8cb023056d76a4ffd85fe7ab374d5466937
David Pérez <dalfonzo18@gmail.com>
2022-08-15 11:18:31 -0400

    Minor changes

--- components/client/about/heading/Heading.tsx
+++ components/client/about/heading/Heading.tsx
@@ -1,8 +1,9 @@
 import { Box, Text } from '@chakra-ui/react'
+import { responsiveProperty } from '~/theme/utils'
 
 const Heading = () => {
   return (
-    <Box my="7rem" as="section">
+    <Box my={responsiveProperty({ desktopSize: 7, mobileSize: 2, unit: 'rem' })} as="section">
       <Text as="h1" variant="title" marginBottom="4rem" textAlign="center">
         Acerca de nosotros
       </Text>
