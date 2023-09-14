97232b2442c793df8ad22e223c589d1e34fd43cb
dap <dalfonzo18@gmail.com>
2023-09-14 17:10:45 -0400

    Adds back btn

--- layouts/Auth.tsx
+++ layouts/Auth.tsx
@@ -1,10 +1,17 @@
 import { Image } from '@chakra-ui/react'
-import { Container, Title } from '@mantine/core'
+import { Button, Container, Title } from '@mantine/core'
+import { IconArrowLeft } from '@tabler/icons-react'
+import { useRouter } from 'next/router'
 
 export default function AuthLayout({ children, title }: { children: React.ReactNode; title: string }) {
+  const router = useRouter()
+
   return (
     <Container w="100%" h="100vh" fluid bg="gray.1">
       <Container size={420} py={40}>
+        <Button leftIcon={<IconArrowLeft />} onClick={() => router.back()} variant="subtle" color="cyan" my="lg">
+          Atrás
+        </Button>
         <Image src="/assets/svg/gotas_de_luz.svg" alt="asd" width={75} height={75} mx="auto" mt="4" mb="5" />
         <Title
           align="center"
