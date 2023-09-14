5694ae922e7d78ddfe8aa923cba6f10358e184bb
dap <dalfonzo18@gmail.com>
2023-09-14 17:00:22 -0400

    Updates auth layout

--- layouts/Auth.tsx
+++ layouts/Auth.tsx
@@ -1,14 +1,20 @@
-import { Box } from '@chakra-ui/react'
-import Footer from '~/components/admin/common/footer/Footer'
-import React from 'react'
+import { Image } from '@chakra-ui/react'
+import { Container, Title } from '@mantine/core'
 
-export default function Auth({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({ children, title }: { children: React.ReactNode; title: string }) {
   return (
-    <Box w="100%">
-      <Box w="100%">{children}</Box>
-      <Box px="24px" mx="auto" width="1044px" maxW="100%">
-        <Footer />
-      </Box>
-    </Box>
+    <Container w="100%" h="100vh" fluid bg="gray.1">
+      <Container size={420} py={40}>
+        <Image src="/assets/svg/gotas_de_luz.svg" alt="asd" width={75} height={75} mx="auto" mt="4" mb="5" />
+        <Title
+          align="center"
+          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+          color="dark"
+        >
+          {title}
+        </Title>
+        {children}
+      </Container>
+    </Container>
   )
 }
