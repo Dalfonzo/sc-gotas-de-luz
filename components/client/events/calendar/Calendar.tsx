4552e1ede2ff781b39a637d12b62c021ced742d0
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2023-09-20 16:58:00 -0400

    FIX: improved calendar layout and issue with loader

--- components/client/events/calendar/Calendar.tsx
+++ components/client/events/calendar/Calendar.tsx
@@ -1,4 +1,5 @@
-import { Flex, useDisclosure } from '@chakra-ui/react'
+import { Alert, AlertIcon, Flex, Text, useDisclosure } from '@chakra-ui/react'
+import { Loader } from '@mantine/core'
 import format from 'date-fns/format'
 import getDay from 'date-fns/getDay'
 import { es } from 'date-fns/locale'
@@ -69,16 +70,33 @@ const MyCalendar = ({ views = { month: true }, defaultView = 'month', ...rest }:
   }
 
   return (
-    <UiFeedback isError={!!error} isLoading={isLoading}>
+    <UiFeedback isError={!!error} >
       {event && <EventModal event={event} visible={isOpen} onClose={onClose} />}
-      <Flex direction="column" justify="center" marginX={['0.5rem', '2rem']}>
+      <Flex direction="column" mx="auto" justify="center" alignItems="center" w="8xl" maxW="95vw">
+        {isLoading && <Loader color="cyan" variant="dots" />}
+        {!isLoading && events && !events.length && (
+          <Alert status="info" marginBottom={5}>
+            <AlertIcon />
+            Aún no hay eventos este mes
+          </Alert>
+        )}
+        {!isLoading && events && events.length > 0 && (
+          <Alert status="info" marginBottom={5}>
+            <AlertIcon />
+            Eventos este mes:{' '}
+            <Text marginLeft={2} fontSize="md" fontWeight="bold">
+              {events.length}
+            </Text>
+          </Alert>
+        )}
+
         <Calendar
           culture="es"
           localizer={localizer}
           events={events}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 500 }}
+          style={{ height: 500, width: '100%' }}
           onSelectEvent={(event) => handleEvent(event as EventI)}
           messages={lang}
           defaultView={defaultView}
